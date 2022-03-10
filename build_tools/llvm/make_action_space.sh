d9eb6c05ecf8f82d9c3a5cbf62e66f9b6ab0005f
Chris Cummins <chrisc.101@gmail.com>
2022-03-10 22:42:00 +0000

    Update build_tools shell script

--- build_tools/llvm/make_action_space.sh
+++ build_tools/llvm/make_action_space.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 #
-# Usage: run.sh <config-py> <llvm-src-root> <outdir>
+# Usage: run.sh <llvm-src-root> <outdir>
 #
 CWD=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 set -euo pipefail
 
 main() {
-    config_py="$1"
-    llvm_root="$2"
-    outdir="$3"
+    llvm_root="$1"
+    outdir="$2"
 
     set -x
     mkdir -p "$outdir"
-    python "$CWD/extract_passes_from_sources.py" --llvm_root "$llvm_root" > "$outdir/passes.json"
+    python "$CWD/extract_passes_from_sources.py" \
+        --llvm_root="$llvm_root" \
+        > "$outdir/passes.json"
     python "$CWD/make_action_space_sources.py" \
-        --outdir "$outdir" \
-        < "$outdir/filtered_passes.json"
+        --outdir="$outdir" \
+        < "$outdir/passpasses.json"
 }
 main $@
